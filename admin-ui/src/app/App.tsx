0f52351ac37b763a883bd2d1babe7ac573733cfc
samlukas <samuelplukas@gmail.com>
2025-02-18 22:25:21 -0500

    Create groups page

--- admin-ui/src/app/App.tsx
+++ admin-ui/src/app/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage, LoginPage } from '@/pages';
 import { Box, ThemePanel } from '@radix-ui/themes';
 import AdminUsersPage from '@/pages/AdminUsersPage';
+import AdminGroupsPage from '@/pages/AdminGroupsPage';
 import '@radix-ui/themes/styles.css';
 import '@/config/fonts.css';
 
@@ -13,6 +14,7 @@ const App = () => {
                 <Route path='/auth/login' element={<LoginPage />} />
                 <Route path='/config/theme' element={<ThemePanel />} />
                 <Route path="/users" element={<AdminUsersPage />} />
+                <Route path="/groups" element={<AdminGroupsPage />} />
             </Routes>
         </Router>
     );
