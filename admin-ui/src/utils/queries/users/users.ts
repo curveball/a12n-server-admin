8ef1766f1b30f427fd48020a5a68ca1212f4c34b
OrhanBC <orhancangurel@gmail.com>
2025-03-04 17:15:18 -0500

    Dynamic user table

--- admin-ui/src/utils/queries/users/users.ts
+++ admin-ui/src/utils/queries/users/users.ts
@@ -8,10 +8,22 @@ export function usersQuery() {
 }
 
 const getUsers = async () => {
-    const res = await fetch('http://localhost:8531/user', {
+    const res = await fetch('http://localhost:8531/user?embed=item', {
         headers: {
             Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
         },
     });
+
+    if (res.status === 401) {
+        // Handle 401 Unauthorized error
+        // For example, redirect to login page or show an error message
+        window.location.href = '/'; // Redirect to login page
+        throw new Error('Unauthorized. Please log in.');
+    }
+
+    if (!res.ok) {
+        throw new Error('Network response was not ok');
+    }
+
     return res.json();
 };
