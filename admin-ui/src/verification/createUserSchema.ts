8c9f4e6cad64f9f942f712cb2ea1dbc28387da74
vivic04 <117671628+vivic04@users.noreply.github.com>
2025-03-07 18:18:14 -0500

    remove

--- admin-ui/src/verification/createUserSchema.ts
+++
@@ -1,9 +0,0 @@
-// src/validation/createUserSchema.ts
-import { z } from 'zod';
-
-export const createUserSchema = z.object({
-  nickname: z.string().min(1, { message: 'Nickname is required' }),
-  email: z.string().email({ message: 'A valid email address is required' }),
-});
-
-export type CreateUser = z.infer<typeof createUserSchema>;
