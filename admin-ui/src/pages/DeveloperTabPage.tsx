ca10b7d90dd88572c00bde0485deb9f00dc89e9a
vivic04 <117671628+vivic04@users.noreply.github.com>
2025-03-07 15:05:24 -0500

    code  cleanup

--- admin-ui/src/pages/DeveloperTabPage.tsx
+++ admin-ui/src/pages/DeveloperTabPage.tsx
@@ -1,27 +1,5 @@
-// import React from 'react';
-// import DeveloperTabComponent from '@/components/DeveloperTabComponent';
-
-// function DeveloperTabPage() {
-//   const baseUrl = import.meta.env.VITE_BASE_URL || 'http://localhost:8531';
-//   const token = import.meta.env.VITE_ACCESS_TOKEN || 'NO_TOKEN_FOUND';
-
-//   return (
-//     <div style={{ maxWidth: '900px', margin: '0 auto', padding: '2rem' }}>
-//       <h1 style={{ marginBottom: '1rem' }}>Developer Tab Page</h1>
-//       <DeveloperTabComponent
-//         fetchUrl="/user?embed=item"
-//         token={token}
-//         baseUrl={baseUrl}
-//       />
-//     </div>
-//   );
-// }
-
-// export default DeveloperTabPage;
-
-
 import React from 'react';
-import DeveloperTabComponent from '@/components/DeveloperTabComponent';
+import DeveloperTabComponent from '../components/DeveloperTabComponent';
 
 function DeveloperTabPage() {
   return (
