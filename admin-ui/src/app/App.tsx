134c7d21ca23df441bc8c37a83534e53fddf66bd
vivic04 <117671628+vivic04@users.noreply.github.com>
2025-03-07 14:35:35 -0500

    changes developer page

--- admin-ui/src/app/App.tsx
+++ admin-ui/src/app/App.tsx
@@ -1,7 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import '@radix-ui/themes/styles.css';
 
-import { OAuthTriggerPage, UserList, OAuthRedirectPage, NotFoundPage } from '../pages';
+import { OAuthTriggerPage, UserList, OAuthRedirectPage, NotFoundPage, DeveloperTabPage } from '../pages';
 import { OAuthProvider } from '../lib/OAuthProvider';
 import { Protected, Layout } from '../components';
 import '../config/theme.css';
@@ -25,6 +25,7 @@ function App() {
                             }
                         />
                     </Route>
+                    <Route path="/developer" element={<DeveloperTabPage />} />
                     <Route path='/404' element={<NotFoundPage />} />
                     <Route path='*' element={<NotFoundPage />} />
                 </Routes>
--- admin-ui/src/pages/index.ts
+++ admin-ui/src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as OAuthRedirectPage } from './OAuthRedirectPage';
 export { default as OAuthTriggerPage } from './OAuthTriggerPage';
 export { default as NotFoundPage } from './NotFoundPage';
 export { default as UserList } from './UserList';
+export {default as DeveloperTabPage} from './DeveloperTabPage';
