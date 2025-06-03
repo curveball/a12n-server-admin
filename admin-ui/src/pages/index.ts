8fdddae86cf8cc869e19ca4fc20e12b7e0f4a81d
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-06-03 10:55:11 -0400

    feat: rename DeveloperTabPage => ApiSandbox, update index file to export it

--- admin-ui/src/pages/index.ts
+++ admin-ui/src/pages/index.ts
@@ -1,8 +1,8 @@
+export { default as ApiSandbox } from './ApiSandbox';
+export { default as AppList } from './AppList';
+export { default as GroupList } from './GroupList';
+export { default as NotFoundPage } from './NotFoundPage';
 export { default as OAuthRedirectPage } from './OAuthRedirectPage';
 export { default as OAuthTriggerPage } from './OAuthTriggerPage';
-export { default as NotFoundPage } from './NotFoundPage';
+export { default as PrivilegeList } from './PrivilegeList';
 export { default as UserList } from './UserList';
-export { default as GroupList} from './GroupList';
-export { default as AppList } from './AppList';
-export { default as DeveloperTabPage } from './DeveloperTabPage';
-export {default as PrivilegeList} from './PrivilegeList'
