0585fd63e91faf0b603a6d2e15c94e61b092c695
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-06-03 18:58:24 -0400

    refactor: update PrivilegeList and UserList components
    - Replace Table component with TableList in PrivilegeList.
    - Rename privilegeTableHeadings to privilegeColumnHeadings for clarity.
    - Introduce userColumnHeadings in UserList with enhanced user data display, including verification and status badges.

--- src/pages/PrivilegeList.tsx
+++ src/pages/PrivilegeList.tsx
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
-import Table from '../components/Table';
+import TableList from '../components/TableList';
 import { useAxios } from '../lib';
 import { privilegesQuery } from '../utils/queries/privileges';
 
-const privilegeTableHeadings = [
+const privilegeColumnHeadings = [
     {
         field: 'title',
         headerName: 'Privilege Title',
@@ -33,9 +33,8 @@ function PrivilegeList() {
         console.log('delete privilege?');
     };
     return (
-        <Table
-            initialValues={[]}
-            columnDefs={privilegeTableHeadings}
+        <TableList
+            columnDefs={privilegeColumnHeadings}
             data={privilegeTableData}
             itemName='privilege'
             onDelete={handleDelete}
