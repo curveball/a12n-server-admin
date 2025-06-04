6e6b2ff41cdc575ea0fed84798020835f8d3cdca
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-06-04 15:59:28 -0400

    refactor: update export paths for AppList and UserList components

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,8 @@
 export { default as ApiSandbox } from './ApiSandbox';
-export { default as AppList } from './AppList';
+export { default as AppList } from './AppList/AppList';
 export { default as GroupList } from './GroupList';
 export { default as NotFoundPage } from './NotFoundPage';
 export { default as OAuthRedirectPage } from './OAuthRedirectPage';
 export { default as OAuthTriggerPage } from './OAuthTriggerPage';
 export { default as PrivilegeList } from './PrivilegeList';
-export { default as UserList } from './UserList';
+export { default as UserList } from './UserList/UserList';
