a2b4b015a84b8c709ca037314616b78719579d03
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-06-03 21:25:09 -0400

    refactor: optimize AppList and GroupList components with useMemo for column definitions
    - Moved column definitions for AppList and GroupList into useMemo for improved performance.
    - This change enhances rendering efficiency by memoizing the column configurations.

--- src/pages/GroupList.tsx
+++ src/pages/GroupList.tsx
@@ -1,58 +1,62 @@
 import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
 import TableList from '../components/TableList';
 import { useAxios } from '../lib';
 import { Groups } from '../utils/helpers/models';
 import { getAllGroups } from '../utils/queries/groups';
 
-const groupColumnHeadings = [
-    {
-        field: 'nickname',
-        headerName: 'Nickname',
-        flex: 1,
-        minWidth: 150,
-        resizable: false,
-    },
-    {
-        field: '_links.self.href',
-        headerName: 'ID',
-        flex: 1,
-        minWidth: 150,
-        resizable: false,
-        valueGetter: (params: any) => Groups.parseGroupID(params.data),
-    },
-    {
-        field: 'modifiedAt',
-        headerName: 'Modified At',
-        flex: 2,
-        minWidth: 250,
-        resizable: false,
-        valueFormatter: (params: any) =>
-            new Date(params.value).toLocaleString(undefined, {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
-            }),
-    },
-    {
-        field: 'createdAt',
-        headerName: 'Created At',
-        flex: 1,
-        minWidth: 250,
-        resizable: false,
-        valueFormatter: (params: any) =>
-            new Date(params.value).toLocaleString(undefined, {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
-            }),
-    },
-];
 const AppList = () => {
     const api = useAxios();
+    const groupColumnHeadings = useMemo(
+        () => [
+            {
+                field: 'nickname',
+                headerName: 'Nickname',
+                flex: 1,
+                minWidth: 150,
+                resizable: false,
+            },
+            {
+                field: '_links.self.href',
+                headerName: 'ID',
+                flex: 1,
+                minWidth: 150,
+                resizable: false,
+                valueGetter: (params: any) => Groups.parseGroupID(params.data),
+            },
+            {
+                field: 'modifiedAt',
+                headerName: 'Modified At',
+                flex: 2,
+                minWidth: 250,
+                resizable: false,
+                valueFormatter: (params: any) =>
+                    new Date(params.value).toLocaleString(undefined, {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                    }),
+            },
+            {
+                field: 'createdAt',
+                headerName: 'Created At',
+                flex: 1,
+                minWidth: 250,
+                resizable: false,
+                valueFormatter: (params: any) =>
+                    new Date(params.value).toLocaleString(undefined, {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                    }),
+            },
+        ],
+        [],
+    );
     const { data, isLoading, error } = useQuery(getAllGroups(api));
 
     if (isLoading) return <div>Loading...</div>;
