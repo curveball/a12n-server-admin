0c7219bd8d745dc29996c5e8c98332a20dba0ecd
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-06-05 19:40:26 -0400

    fix: update authentication test to use explicit URL and improve e2e setup script for directory creation

--- src/tests/auth.setup.ts
+++ src/tests/auth.setup.ts
@@ -6,7 +6,7 @@ const authFile = 'playwright/.auth/user.json';
 setup('authenticate', async ({ page }) => {
     await page.goto(CLIENT_ROUTES.AUTH_TRIGGER);
 
-    await expect(page).toHaveURL(new RegExp(`^${process.env.VITE_SERVER_URL!}`));
+    await expect(page.url()).toContain(`http://localhost:5173/`);
 
     await page.fill('input[name="userName"]', process.env.VITE_AUTH_SERVER_EMAIL!);
     await page.fill('input[name="password"]', process.env.VITE_AUTH_SERVER_PASSWORD!);
