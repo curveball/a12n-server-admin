5d3762cb39370fac2b5340ac7ac9915a1107b08c
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-06-05 19:40:26 -0400

    fix: update authentication test to use explicit URL and improve e2e setup script for directory creation

--- src/tests/e2e-setup.sh
+++ src/tests/e2e-setup.sh
@@ -1,16 +1,20 @@
 #! /usr/bin/bash
 
 echo "Creating a12n-server directory...";
-mkdir src/tests/a12n-server && cd src/tests/a12n-server;
+if [ ! -d "src/tests/a12n-server" ]; then
+    mkdir src/tests/a12n-server;
+fi
 
 echo "Moving snapshot.sql to a12n-server...";
-cp ../snapshot.sql .
+cp snapshot.sql .
+cd src/tests/a12n-server;
 
 echo "Restoring database...";
-sqlite3 a12nserver.sqlite3 < snapshot.sql
+sqlite3 a12nserver.sqlite3 < ../snapshot.sql
 
 touch .env;
 echo "DB_DRIVER=sqlite3" >> .env;
 echo "DB_FILENAME=a12nserver.sqlite3" >> .env;
 echo "CURVEBALL_ORIGIN=http://localhost:8531" >> .env;
+echo "PORT=8531" >> .env;
 npx @curveball/a12n-server;
