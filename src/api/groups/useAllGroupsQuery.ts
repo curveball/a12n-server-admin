fe74ba790b7257fd30d465f831c24cc0bbbe0505
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-06-11 15:30:22 -0400

    refactor: enhance useAllGroupsQuery hook to include group count
    - Added a new state variable `groupCount` to track the number of groups fetched.
    - Updated the return object of the `useAllGroupsQuery` hook to include `groupCount`, improving data accessibility for components using this hook.

--- src/api/groups/useAllGroupsQuery.ts
+++ src/api/groups/useAllGroupsQuery.ts
@@ -8,16 +8,17 @@ const useAllGroupsQuery = () => {
     const api = useAxios();
     const queryOptions = getAllGroups(api);
     const [groups, setGroups] = useState<HalLink[]>([]);
-
+    const [groupCount, setGroupCount] = useState(0);
     const { data, isLoading, error } = useQuery(queryOptions);
 
     useEffect(() => {
         if (data) {
             setGroups(data?._links?.item as unknown as HalLink[]);
+            setGroupCount(data?.length ?? 0);
         }
     }, [data]);
 
-    return { data, groups, isLoading, error };
+    return { data, groups, groupCount, isLoading, error };
 };
 
 export default useAllGroupsQuery;
