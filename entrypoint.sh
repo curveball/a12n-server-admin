eca9b4f3b21053d7cfe7ef746e731ca772f51a37
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-06-16 19:13:03 -0400

    chore: add build arguments for environment variables in Docker setup for Playwright tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 set -e
 
-chown pwuser:pwuser /home/pwuser/src/tests/
 # Ensure directory exists and is writable
 mkdir -p /home/pwuser/src/tests/a12n-server
+chown pwuser:pwuser /home/pwuser/src/tests/
 
 # Remove directory if it exists (from previous runs)
 if [ -d /home/pwuser/src/tests/a12n-server/a12nserver.sqlite3 ]; then
   echo "Removing directory named a12nserver.sqlite3"
   rm -rf /home/pwuser/src/tests/a12n-server/a12nserver.sqlite3
 fi
+
+chmod +wx /home/pwuser/src/tests/a12n-server/a12nserver.sqlite3 /home/pwuser/src/tests/a12n-server/.env
 
 # Initialize DB if not present
 if [ ! -f /home/pwuser/src/tests/a12n-server/a12nserver.sqlite3 ]; then
