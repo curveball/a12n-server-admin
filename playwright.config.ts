43afe02806cbcdd3876c0960e3a6a8508dcc6b40
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-06-17 14:39:40 -0400

    ci: Add Playwright dependencies installation step in workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,6 @@ export default defineConfig({
             use: {
                 baseURL: 'http://localhost:5173',
                 headless: process.env.CI ? true : false,
-                httpCredentials: {
-                    username: process.env.VITE_AUTH_SERVER_EMAIL || '',
-                    password: process.env.VITE_AUTH_SERVER_PASSWORD || '',
-                },
             },
         },
     ],
