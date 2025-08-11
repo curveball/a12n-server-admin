f45f55dce241c6dcb0b0ae1f7223f3681b88cde5
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-08-11 11:23:05 -0400

    refactor: remove unused props from ApiSandbox in SandboxView component

--- src/pages/SandboxView.tsx
+++ src/pages/SandboxView.tsx
@@ -1,22 +1,10 @@
 import ApiSandbox from '../components/ApiSandbox/ApiSandbox';
-import useSandboxQueries from '../hooks/useSandboxQueries';
 
 const SandboxView = () => {
-    const { method, fullUrl, requestRoute, setRequestRoute, isFetching, data, error, refetch } = useSandboxQueries();
-
     return (
         <div className='max-w-7xl mx-auto p-4'>
             <h1 className='text-2xl font-bold mb-4'>Developer Tab Page</h1>
-            <ApiSandbox
-                method={method}
-                fullUrl={fullUrl}
-                requestRoute={requestRoute ?? ''}
-                setRequestRoute={setRequestRoute}
-                isFetching={isFetching}
-                data={data}
-                error={error}
-                refetch={refetch}
-            />
+            <ApiSandbox />
         </div>
     );
 };
